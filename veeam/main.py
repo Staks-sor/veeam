df5af1148a14882ea5fb917bf2ff777437edde94
stass <stas.sor@gmail.com>
2021-10-16 19:54:35 +0300

    второй прототип

--- veeam/main.py
+++ veeam/main.py
@@ -1,6 +1,18 @@
 # pip install psutil
 import psutil
+import time
 
-cpu = psutil.cpu_percent() # Вывод работы процессора
-memory_my = psutil.virtual_memory() # Вывод работы оперативной памяти
-print('\nВывод процессора -', cpu, '\nВывод памяти -', memory_my)
+TIME_TO_LOOP = 60
+start = time.time()
+while time.time() < start + TIME_TO_LOOP:
+    start = time.time()
+    cpu = psutil.cpu_percent(interval=1) # Вывод загрузки процессора в роцентах
+    working_set = psutil.virtual_memory().used
+    private_bytes = psutil.virtual_memory().percent # Вывод работы оперативной памяти
+    #for proc in psutil.process_iter(['pid', 'name', 'username']):
+    print("\nЗагрузка процессора - {}% \nИспользование памяти - {} \nПамять - {}%".format(cpu, working_set, private_bytes))
+time.sleep(5)
+
+
+
+
