d0b4e8b937bccfff03a1e5144c27a9cdc5a9b4b6
Stanislav <stas.sor@gmail.com>
2021-10-18 15:39:14 +0300

    Слияние состоялось добавил лог, пока кривовато все.

--- veeam/merger.py
+++ veeam/merger.py
@@ -1,55 +1,74 @@
+import sys
+import time
 import PySimpleGUI as sg
 import psutil
-import time
-import sys
+import win32api
+import win32con
+import datetime
+import json
+import ctypes
 
-# This bit gets the taskbar icon working properly in Windows
 if sys.platform.startswith("win"):
-    import ctypes
+
 
     ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(
         u"CompanyName.ProductName.SubProduct.VersionInformation"
     )
 sg.theme("Dark Brown")
 
-
-
-
-
 layout = [
-    [sg.Text("Введите интервал проверки"), sg.InputText(key='-INPUT-', size=(10, 10))],
+    [
+        sg.Text("Введите интервал проверки в секундах"),
+        sg.InputText(key="-INPUT-", size=(10, 10)),
+    ],
     [sg.Output(size=(88, 20))],
     [sg.Submit("Начать"), sg.Exit("закрыть")],
 ]
 
 window = sg.Window(
-    "Veeam homework",
+    "Test exercise",
     layout,
     resizable=False,
     finalize=True,
     icon="C:/Users/stass/OneDrive/Pictures/Thesquid.ink-Free-Flat-Sample-Space-rocket.ico",
 )
-def executeSomething():
 
+
+def executeSomething():
     cpu = psutil.cpu_percent(interval=1)  # Вывод загрузки процессора в роцентах
-    private_bytes = psutil.virtual_memory().used # Вывод работы оперативной памяти
+    private_bytes = psutil.virtual_memory().used  # Вывод работы оперативной памяти
     working_set = psutil.virtual_memory().percent  # Вывод работы оперативной памяти
+    handle = psutil.Process().pid
+    current_time = datetime.datetime.now().strftime("%F %T")
+    log_str = "|---------------------|---------- |-----------------|-----------------|-----------------------------|\n"
+    log_str += "|---------time--------|----cpu----|--private_bytes--|---working_set---|-------------handle----------|\n"
+
+    log_str += (
+        "| %s |   %s%%    |   %s   |%s%%            | %s                       |\n"
+        % (current_time, cpu, private_bytes, working_set, handle)
+    )
+    print(log_str)
+    f = open("log.json", "a")  # Новый log.txt
+    f.write(log_str + "\n\n")  # Написать информацию
+    f.close()
 
     print(
-        "\nЗагрузка процессора - {}% \nИспользование памяти - {}% \nПамять - {}".format(
-            cpu, working_set, private_bytes
+        "\nЗагрузка процессора - {}% \nИспользование памяти - {}% \nПамять - {} \nОткрытые хендлы - {}".format(
+            cpu, working_set, private_bytes, handle
         )
     )
-    number = values['-INPUT-']
+    number = values["-INPUT-"]
+
     time.sleep(int(number))
     while True:
         executeSomething()
 
+
 while True:
     event, values = window.Read()
-    if event in (None, "Exit"):
+    if event in (None, "Exit", 'закрыть'):
         break
     if event == "Начать":
         executeSomething()
 
-windows.close()
+windows.quit()
