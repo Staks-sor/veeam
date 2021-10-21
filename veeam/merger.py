46008fe99f2ddaa4132978f3e392490274a3d950
Stanislav <stas.sor@gmail.com>
2021-10-21 08:57:40 +0300

    наработки по слиянию интерфейса, возможности программы.

--- veeam/merger.py
+++ veeam/merger.py
@@ -2,69 +2,32 @@ import sys
 import time
 import PySimpleGUI as sg
 import psutil
-import win32api
-import win32con
 import datetime
-import ctypes
-import base64
 
 
-if sys.platform.startswith("win"):
-
-
-    ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(
-        u"CompanyName.ProductName.SubProduct.VersionInformation"
-    )
-sg.theme("Dark Brown")
-
-layout = [
-    [
-        sg.Text("Введите интервал проверки в секундах"),
-        sg.InputText(key="-INPUT-", size=(10, 10)),
-    ],
-    [sg.Output(size=(88, 20))],
-    [sg.Submit("Начать"), sg.Exit("закрыть")],
-]
-
-window = sg.Window(
-    "Test exercise",
-    layout,
-    resizable=False,
-    finalize=True,
-    icon="C:/Users/stass/OneDrive/Pictures/Thesquid.ink-Free-Flat-Sample-Space-rocket.ico",
-)
-
-
-def executeSomething():
+#number = int(input())
+time.sleep(int(input()))
+while True:
     cpu = psutil.cpu_percent(interval=1)  # Вывод загрузки процессора в роцентах
     private_bytes = psutil.virtual_memory().used  # Вывод работы оперативной памяти
     working_set = psutil.virtual_memory().percent  # Вывод работы оперативной памяти
-    handle = psutil.Process().pid
+    pid = psutil.Process().pid
     current_time = datetime.datetime.now().strftime("%F %T")
-    log_str = " time            {}\n cpu             {}%\n private_bytes   {}\n working_set     {}%\n handle          {}\n".format(current_time, cpu, private_bytes, working_set, handle)
+    log_str = " time            {}\n cpu             {}%\n private_bytes   {}\n working_set     {}%\n handle          {}\n".format(current_time, cpu, private_bytes, working_set, pid)
+
+    print(log_str)
+    metrics_data = {
+        "cpu": cpu,
+        "private_bytes": private_bytes,
+        "working_set": working_set,
+        "pid": pid,
+        "current_time": current_time,
+    }
+
+    with open("log/data.json", "a") as data_file:
+        json.dump(metrics_data, data_file, indent=2)
 
 
-    f = open("log.txt", "a")  # Новый log.txt
-    f.write(log_str + "\n\n")  # Написать информацию
-    f.close()
-
-    print(
-        "\nЗагрузка процессора - {}% \nИспользование памяти - {}% \nПамять - {} \nОткрытые хендлы - {}".format(
-            cpu, working_set, private_bytes, handle
-        )
-    )
-    number = values["-INPUT-"]
-
-    time.sleep(int(number))
-    while True:
-        executeSomething()
 
 
-while True:
-    event, values = window.Read()
-    if event in (None, "Exit", 'закрыть'):
-        break
-    if event == "Начать":
-        executeSomething()
 
-windows.quit()
