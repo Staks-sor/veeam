707403a2d05a8d5a7205ee442766d661e4f43e23
Stanislav <stas.sor@gmail.com>
2021-10-21 08:56:44 +0300

    обычный варинт без графической оболочки

--- veeam/main.py
+++ veeam/main.py
@@ -1,97 +1,30 @@
-import sys
-import time
-import PySimpleGUI as sg
 import psutil
-import win32api
-import win32con
+import time
 import datetime
-import ctypes
-import base64
-import threading
+import json
 
-if sys.platform.startswith("win"):
-
-    ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(
-        u"CompanyName.ProductName.SubProduct.VersionInformation"
-    )
-sg.theme("Dark Brown")
-
-
-class win(threading.Thread):
-    def __init__(self, name, counter):
-        threading.Thread.__init__(self)
-        self.threadID = counter
-        self.name = name
-        self.counter = counter
-
-
-    def run(self):
-
-        layout = [
-            [
-                sg.Text("Введите интервал проверки в секундах", key='-IN-'),
-                sg.InputText(key='-INS-', size=(10, 10)),
-            ],
-            [sg.Output(size=(88, 20))],
-            [sg.Submit("Начать"), sg.Exit("закрыть")],
-        ]
-
-
-        window = sg.Window(
-            "Test exercise",
-            layout,
-            resizable=False,
-            finalize=True,
-            icon="C:/Users/stass/OneDrive/Pictures/Thesquid.ink-Free-Flat-Sample-Space-rocket.ico",
-        )
-
-
-        while True:
-            event, values = window.Read()
-            if event in (None, "Exit", "закрыть"):
-                break
-
-            if event == "Начать":
-                thread1 = st("Thread1", 1)
-                thread1.start()
+number = int(input("Ведите промижуток времени проверки в секундах: "))
 
 
 
+while True:
+    time.sleep(number)
+    cpu = psutil.cpu_percent(interval=1)  # Вывод загрузки процессора в роцентах
+    private_bytes = psutil.virtual_memory().used  # Вывод работы оперативной памяти
+    working_set = psutil.virtual_memory().percent  # Вывод работы оперативной памяти
+    pid = psutil.Process().pid
+    current_time = datetime.datetime.now().strftime("%F %T")
+    log_str = " time            {}\n cpu             {}%\n private_bytes   {}\n working_set     {}%\n handle          {}\n".format(
+        current_time, cpu, private_bytes, working_set, pid)
+    print(log_str)
+    metrics_data = {
+        "cpu": cpu,
+        "private_bytes": private_bytes,
+        "working_set": working_set,
+        "pid": pid,
+        "current_time": current_time,
+    }
 
+    with open("data.json", "a") as data_file:
+        json.dump(metrics_data, data_file, indent=2)
 
-
-
-
-class st(win):
-
-    def run(self):
-
-        while True:
-            cpu = psutil.cpu_percent(interval=1)  # Вывод загрузки процессора в роцентах
-            private_bytes = (
-                psutil.virtual_memory().used
-            )  # Вывод работы оперативной памяти
-            working_set = (
-                psutil.virtual_memory().percent
-            )  # Вывод работы оперативной памяти
-            handle = psutil.Process().pid
-            current_time = datetime.datetime.now().strftime("%F %T")
-            log_str = " time            {}\n cpu             {}%\n private_bytes   {}\n working_set     {}%\n handle          {}\n".format(
-                current_time, cpu, private_bytes, working_set, handle
-            )
-
-            f = open("log.txt", "a")  # Новый log.txt
-            f.write(log_str + "\n\n")  # Написать информацию
-            f.close()
-
-            print(
-                "\nЗагрузка процессора - {}% \nИспользование памяти - {}% \nПамять - {} \nОткрытые хендлы - {}".format(
-                    cpu, working_set, private_bytes, handle
-                )
-            )
-
-
-            time.sleep(int(2))
-thread0 = win("Thread0", 0)
-thread0.start()
-window.close()
